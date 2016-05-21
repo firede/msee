a549d2c7ba51feca4706c4c2e988252a84156735
Martin Heidegger <martin.heidegger@gmail.com>
2016-05-21 16:25:16 +0900

    Added linux support in tests

--- test/msee.js
+++ test/msee.js
@@ -2,11 +2,13 @@ const test = require('tap').test
 const msee = require('../')
 const path = require('path')
 const fs = require('fs')
+const os = require('os')
 
 test('default general test', function (t) {
+	var platform = os.platform() === 'linux' ? '.linux' : ''
 	t.equal(
 		msee.parseFile(path.join(__dirname, 'fixtures', 'general.md'), {maxWidth: 80}),
-		fs.readFileSync(path.join(__dirname, 'fixtures', 'general.out'), 'utf8')
+		fs.readFileSync(path.join(__dirname, 'fixtures', 'general' + platform + '.out'), 'utf8')
 	)
 	t.end()
 })
