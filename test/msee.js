76dc2eac903df8123453a280e8ca725045069f33
Martin Heidegger <martin.heidegger@gmail.com>
2016-05-21 21:45:33 +0900

    Fixed grey color in ubuntu (#36)
    * Fixed grey color in ubuntu
    
    * Added linux support in tests
    
    * Test platform output
    
    * Fixing line-break issues.

--- test/msee.js
+++ test/msee.js
@@ -2,11 +2,15 @@ const test = require('tap').test
 const msee = require('../')
 const path = require('path')
 const fs = require('fs')
+const os = require('os')
 
 test('default general test', function (t) {
+	var platform = os.platform() === 'linux' ? '.linux' : ''
+	var pth = 'general' + platform + '.out'
+	var out = fs.readFileSync(path.join(__dirname, 'fixtures', pth), 'utf8')
 	t.equal(
 		msee.parseFile(path.join(__dirname, 'fixtures', 'general.md'), {maxWidth: 80}),
-		fs.readFileSync(path.join(__dirname, 'fixtures', 'general.out'), 'utf8')
+		out
 	)
 	t.end()
 })
