6ead70fb24c4488f5a06f0bafbf0d2b1466a5d66
Martin Heidegger <martin.heidegger@gmail.com>
2016-05-21 17:20:27 +0900

    Fixing line-break issues.

--- test/msee.js
+++ test/msee.js
@@ -6,10 +6,11 @@ const os = require('os')
 
 test('default general test', function (t) {
 	var platform = os.platform() === 'linux' ? '.linux' : ''
-	console.log(platform)
+	var pth = 'general' + platform + '.out'
+	var out = fs.readFileSync(path.join(__dirname, 'fixtures', pth), 'utf8')
 	t.equal(
 		msee.parseFile(path.join(__dirname, 'fixtures', 'general.md'), {maxWidth: 80}),
-		fs.readFileSync(path.join(__dirname, 'fixtures', 'general' + platform + '.out'), 'utf8')
+		out
 	)
 	t.end()
 })
