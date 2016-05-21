b623293045d0639931e2fb1974864a4a3a2c4757
Martin Heidegger <martin.heidegger@gmail.com>
2016-05-21 16:30:43 +0900

    Test platform output

--- test/msee.js
+++ test/msee.js
@@ -6,6 +6,7 @@ const os = require('os')
 
 test('default general test', function (t) {
 	var platform = os.platform() === 'linux' ? '.linux' : ''
+	console.log(platform)
 	t.equal(
 		msee.parseFile(path.join(__dirname, 'fixtures', 'general.md'), {maxWidth: 80}),
 		fs.readFileSync(path.join(__dirname, 'fixtures', 'general' + platform + '.out'), 'utf8')
