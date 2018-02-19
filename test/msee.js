ded8a1f141f6b171f7a2da03a1c13f0e13764525
Martin Heidegger <martin.heidegger@gmail.com>
2018-02-19 11:19:34 +0900

    fix: Fixing multiple for all platforms

--- test/msee.js
+++ test/msee.js
@@ -5,13 +5,13 @@ const fs = require('fs')
 const os = require('os')
 const cli = require('../lib/cli')
 const streamToString = require('stream-to-string')
+const platform = os.platform() === 'linux' ? '.linux' : ''
 
 function fixture (fileName) {
 	return path.join(__dirname, 'fixtures', fileName)
 }
 
 test('default general test', function (t) {
-	var platform = os.platform() === 'linux' ? '.linux' : ''
 	var out = fs.readFileSync(fixture('general' + platform + '.out'), 'utf8')
 	t.equal(
 		msee.parseFile(fixture('general.md'), {maxWidth: 80}),
@@ -26,7 +26,7 @@ test('multifile test', function (t) {
 		fixture('multifile-first.md'),
 		fixture('multifile-second.md')
 	]), function (err, msg) {
-		var out = fs.readFileSync(fixture('multifile.out'), 'utf8')
+		var out = fs.readFileSync(fixture('multifile' + platform + '.out'), 'utf8')
 		t.equal(err, null)
 		t.equal(msg, out)
 		t.end()
